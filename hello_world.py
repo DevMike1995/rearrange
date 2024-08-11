3cbca558857bac057404fe7948f9a5e79db70a0f
mmarchetti <mikemarchetti95@yahoo.it>
2024-08-11 14:44:32 +0200

    Added a input request for the name that will be greet

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,7 @@
 def saluta():
-        print('Hello World!')
+    '''Function that ask user to type a name to say hello '''
+    name = input('Enter a name: \t')
+    print(f'Hello {name}!')
 
 
 if __name__ == '__main__':
